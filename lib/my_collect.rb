0a104f8819a86c51e7496d79637bc1106801b34c
Muhammad <muhammadhullah23@gmail.com>
2021-03-05 16:40:08 -0500

    commit tree b2090a516a60e444505b9392a1d4d5a189039e83

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(languages)
+    i = 0
+    upcase_languages = []
+    while i < languages.length
+        upcase_languages << yield(languages[i])
+        i += 1
+    end
+    upcase_languages
+end
